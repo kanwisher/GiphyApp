c7244bf0c0c55f62655b08551e81b901780cd47a
kanwisher <kanwisher@gmail.com>
2017-01-17 14:44:49 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,7 +70,7 @@ $(function() {
         $.ajax({
 
             //sets limit to 10 in url
-            url: "HTTPS://api.giphy.com/v1/gifs/search?q=" + $(this).text() + "&api_key=dc6zaTOxFJmzC&limit=10",
+            url: "https//api.giphy.com/v1/gifs/search?q=" + $(this).text() + "&api_key=dc6zaTOxFJmzC&limit=10",
             type: "GET",
             success: function(response) {
             	console.log(response);
